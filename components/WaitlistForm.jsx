1ff238ac2bb09c3c9b325819b2797177a767d101
Damian <olebueziedamian@gmail.com>
2023-12-30 09:53:08 +0100

    3odec

--- components/WaitlistForm.jsx
+++ components/WaitlistForm.jsx
@@ -70,16 +70,6 @@ const WaitlistForm = () => {
           innovations
         </p>
       </div>
-    <section
-      id="waitlist"
-      className="max-w-[800px] mx-auto p-4 rounded-lg flex flex-col md:flex-row justify-center gap-2 items-center bg-gradient-to-br from-slate-900 to-slate-950">
-      <div className="p-4 text-center md:text-left">
-        <p className="text-3xl mb-2 font-bold">GM Fren!</p>
-        <p>
-          Subscribe to get exclusive access to groundbreaking features and
-          innovations
-        </p>
-      </div>
       <form action="#" method="post" onSubmit={submitEmail}>
         <div className="text-red-500 text-sm text-center">{error}</div>
         <div className="text-green-500 text-sm text-center">{successMsg}</div>
